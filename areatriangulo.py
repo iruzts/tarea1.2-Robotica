14ea25333d6292bad46f325cb1dbad3fdf6c88f0
iruzts <ruizsoto1996@gmail.com>
2021-05-29 18:43:24 -0600

    Programa calculo del area de un triangulo

---
+++ areatriangulo.py
@@ -0,0 +1,7 @@
+print("Calculo del area de un Triangulo")
+base = int(input("ingrese la base: " ) )
+altura = int(input("ingrese la altura: "))
+
+area=base*altura/2
+
+print(f"El area del Triangulo es: {base*altura/2} ")
